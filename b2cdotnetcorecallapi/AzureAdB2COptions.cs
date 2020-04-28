5aa476ad85a32641afa34f7e24f658323416f952
Brett Hacker <bretthackermsft@users.noreply.github.com>
2020-04-27 23:21:41 -0500

    initial checkin

---
+++ b2cdotnetcorecallapi/AzureAdB2COptions.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp_OpenIDConnect_DotNet
+{
+    public class AzureAdB2COptions
+    {
+        public const string PolicyAuthenticationProperty = "Policy";
+        public string TenantUrl => $"https://{Tenant}.b2clogin.com";
+        public string AzureAdB2CInstance => $"{TenantUrl}/tfp";
+        public string ClientId { get; set; }
+        public string Tenant { get; set; }
+        public string SignUpSignInPolicyId { get; set; }
+        public string SignInPolicyId { get; set; }
+        public string SignUpPolicyId { get; set; }
+        public string ResetPasswordPolicyId { get; set; }
+        public string EditProfilePolicyId { get; set; }
+        public string CallbackPath { get; set; }
+        public string DefaultPolicy => SignUpSignInPolicyId;
+        public string Authority => $"{AzureAdB2CInstance}/{Tenant}.onmicrosoft.com/{DefaultPolicy}/v2.0";
+        //public string Authority => $"{TenantUrl}/{Tenant}.onmicrosoft.com/{DefaultPolicy}/v2.0";
+
+
+        //B2C 'authority' Uri should have at least 3 segments in the path (i.e. https://<host>/tfp/<tenant>/<policy>/...)
+
+        public string ClientSecret { get; set; }
+        public string ApiUrl { get; set; }
+        public string ApiScopes { get; set; }
+
+        public static string GetRedirectUrl(HttpRequest request, string callbackPath)
+        {
+            var cloudHost = Environment.GetEnvironmentVariable("WEBSITE_HOSTNAME");
+
+            string appBaseUrl = string.Format("{0}://{1}{2}", request.Scheme, (cloudHost ?? request.Host.ToString()), request.PathBase);
+            return appBaseUrl + callbackPath;
+        }
+    }
+}
---
+++ b2cdotnetcorecallapi/Startup.cs
@@ -0,0 +1,85 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.AzureADB2C.UI;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using WebApp_OpenIDConnect_DotNet;
+
+namespace b2cdotnetcorecallapi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
+            services.AddAuthentication(sharedOptions =>
+            {
+                sharedOptions.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                sharedOptions.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+            })
+            .AddAzureAdB2C(options => Configuration.Bind("Authentication:AzureAdB2C", options))
+            .AddCookie();
+
+            // Add framework services.
+            services.AddMvc(config =>
+            {
+                config.EnableEndpointRouting = false;
+            });
+
+            // Adds a default in-memory implementation of IDistributedCache.
+            services.AddDistributedMemoryCache();
+            services.AddSession(options =>
+            {
+                options.IdleTimeout = TimeSpan.FromHours(1);
+                options.Cookie.HttpOnly = true;
+                options.Cookie.IsEssential = true;
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
